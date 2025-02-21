45767cbbe27cb9d8819ba40d93b78c34b0d15f26
JayantiRastogi <160047706+JayantiRastogi@users.noreply.github.com>
2025-02-21 21:08:04 +0530

    sub_update
    updating sub func

--- func1.py
+++ func1.py
@@ -2,7 +2,7 @@ def add(a,b):
     return a+b
 
 
-def sub(a,b):
-    return a-b
+def sub(a,b,c):
+    return a-b-c
 
-print("this file performs addition and subtractiuon for two numbers")
+print("this file performs addition and subtractiuon for three numbers")
